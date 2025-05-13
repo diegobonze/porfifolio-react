7dd358d4cc0da7c7782f06be50e2521e8d4dba45
Diego Bonze <diegobonze747@gmail.com>
2025-05-13 14:56:56 -0300

    update portfolio

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -34,12 +34,13 @@ export const Projects = () => {
                 <a
                   href="https://github.com/diegobonze/fincheck"
                   target="_blank"
-                  className="text-blue-400 hover:text-blue-300 transition-colors mt-2 mb-4"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   View project on GitHub →
                 </a>
               </div>
             </div>
+
             <div
               className="
               glass p-6 rounded-xl border border-white/10 
@@ -78,6 +79,52 @@ export const Projects = () => {
                 </a>
                 <a
                   href="https://github.com/diegobonze/IgniteTimer"
+                  target="_blank"
+                  className="text-blue-400 hover:text-blue-300 transition-colors mt-2 mb-4"
+                >
+                  View project on GitHub →
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">Explorer Lab #01</h3>
+              <p className="text-gray-400 mb-4">
+                Rocketpay is a component that simulates a credit card form, allowing input masking and updating HTML elements via the DOM. The project was developed as part of the Explorer Lab program, which aims to provide practical experience in web development.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["HTML and CSS", "JavaScript e JSON", "TypeScript"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex flex-col">
+                <a
+                  href="https://explorer-lab-neon-nine.vercel.app/"
+                  target="_blank"
+                  className="text-blue-400 hover:text-blue-300 transition-colors mt-4 mb-2"
+                >
+                  View Project →
+                </a>
+                <a
+                  href="https://github.com/diegobonze/explorer-lab"
                   target="_blank"
                   className="text-blue-400 hover:text-blue-300 transition-colors mt-2 mb-4"
                 >
